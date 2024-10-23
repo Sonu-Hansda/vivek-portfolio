85f49b270d83b3e6fec680326f4ca3f627d9ce26
Sonu-Hansda <sonukumarhansda61@gmail.com>
2024-10-23 11:55:31 +0530

    removed tech animation

--- src/components/Technology.tsx
+++ src/components/Technology.tsx
@@ -1,5 +1,3 @@
-import { motion } from 'framer-motion';
-
 function Technology() {
   const images = [
     { src: "https://www.adobe.com/cc-shared/assets/img/product-icons/svg/premiere-pro.svg", alt: "Adobe Premiere Pro" },
@@ -14,33 +12,19 @@ function Technology() {
       <div className="mx-auto text-center mb-6">
         <h2 className="text-gray-600 md:text-2xl">" In the world of video, the editor is the storyteller. "</h2>
       </div>
-      <div className="relative py-6 md:py-10 overflow-hidden before:bg-gradient-to-r before:from-white before:to-transparent after:bg-gradient-to-l after:from-white after:to-transparent before:absolute before:top-0 before:start-0 before:z-10 before:w-20 before:h-full after:absolute after:top-0 after:end-0 after:w-20 after:h-full">
-        <div className="flex overflow-hidden">
-          <motion.div
-            className="flex"
-            animate={{
-              x: ['20%', '-30%', '20%']
-            }}
-            transition={{ duration: 20, ease: "linear", times: [0, 0.5, 1], repeat: Infinity }}
+      <div className="relative py-6 md:py-10 overflow-hidden before:absolute before:top-0 before:start-0 before:z-10 before:w-20 before:h-full after:absolute after:top-0 after:end-0 after:w-20 after:h-full">
+          <div
+            className="flex justify-center items-center gap-x-4"
           >
             {images.map((image, index) => (
               <img
                 key={index}
-                className="w-12 md:w-16 h-auto mr-2 md:mr-8"
+                className="w-12 md:w-16 h-auto"
                 src={image.src}
                 alt={image.alt}
               />
             ))}
-            {images.map((image, index) => (
-              <img
-                key={`duplicate-${index}`}
-                className="hidden md:block w-12 md:w-16 h-auto mr-8"
-                src={image.src}
-                alt={image.alt}
-              />
-            ))}
-          </motion.div>
-        </div>
+          </div>
       </div>
     </div>
   );
